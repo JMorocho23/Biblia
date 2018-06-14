dac10ae3e6fe84a68c69db7541b9d6ffd13aa42d
Juan Morocho <jc23ace@gmail.com>
2018-06-14 12:27:03 -0500

    Areglado UX

--- src/services/biblia-list.service.ts
+++ src/services/biblia-list.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 import { AngularFirestore, AngularFirestoreCollection, 
 AngularFirestoreDocument } from "angularfire2/firestore";
 import { Observable } from 'rxjs/Observable';
-
 import { Cita } from './../models/item.model'
 
 
